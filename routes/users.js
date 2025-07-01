4c7bce2a89349d8672051e240470339ba49a28ad
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-01 12:55:50 +0200

    Structured users router with clean chained route handlers for :id

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,18 @@ router.get("/new", (req, res)=>{
 router.post("/", (req,res)=>{
         res.send("Create New user")
 })    
-router.get("/:id", (req,res)=>{
+// Alternative clean Express code
+router.route("/:id")
+.get((req,res)=>{
        res.send(`Get user with ID ${req.params.id}`)
 })
+.put((req,res)=>{
+       res.send(`Update user with ID ${req.params.id}`)
+})
+.delete((req,res)=>{
+       res.send(`delete user with ID ${req.params.id}`)
+})
+
+
 
 module.exports = router;    

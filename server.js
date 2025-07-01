a2a56c7bea78d724e3b949017f921145aa61c574
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-01 11:51:27 +0200

    Added EJS view rendering with dynamic text passed from server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req,res) =>{
     console.log('Here');
-    res.status(500).send('Hi')
+    res.render("index", {text: "World"})   // install library by npm i ejs
+                          // render method takes two parameter
+                          // passing text from server to ejs
+
+    //res.download("server.js")
+    //res.json({ message: "Error"})  // json message to the server 
     //res.send("Hi") //it sends to the data to the client
 })
 

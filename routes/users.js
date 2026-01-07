d12e592ceac01e30b0574a357ba55da38f8f16d1
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-07 22:43:39 +0100

    added dynamic user handling via route parameters

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get("/new", (req, res)=>{
 router.post("/", (req,res)=>{
         res.send("Create New user")
 })    
-// Alternative clean Express code    kommit
+// Alternative clean Express code    kommi
 router.route("/:id")
 .get((req,res)=>{
        console.log(req.user);

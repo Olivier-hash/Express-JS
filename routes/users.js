afdf078f5fc029ed5365994f8fa60a3a15893265
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-08 20:21:03 +0100

    preload user data using route parameter middleware

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get("/new", (req, res)=>{
 router.post("/", (req,res)=>{
         res.send("Create New user")
 })    
-// Alternative clean Express code    ko
+// Alternative clean Express code
 router.route("/:id")
 .get((req,res)=>{
        console.log(req.user);
